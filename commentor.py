42fdd86e6ed8dca4803ddb6f583d9033b9d845fb
Philip Peterson <philip.c.peterson@gmail.com>
2014-02-22 01:24:57 -0500

    Initial commit

---
+++ commentor.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://localhost/philippeterson'
+db = SQLAlchemy(app)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    refcode = db.Column(db.String(255), unique=True)
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    post_parent = db.ForeignKey('post.id')
+    user = db.ForeignKey('user.id')
+    approved = db.Column(db.Boolean)
+
+    parent = db.ForeignKey('comment.id')
+    text = db.Column(db.String(65535))
+    date = db.Column(db.DateTime)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255))
+    displayName = db.Column(db.String(255))
+    hash = db.Column(db.String(41))
+    email = db.Column(db.String(255))
+    join_date = db.Column(db.DateTime)
+    verified = db.Column(db.Boolean)
+
+if __name__ == '__main__':
+   app.run()
---
+++ seed_db.py
@@ -0,0 +1,2 @@
+from commentor import db
+db.create_all()
